c8fa4b8d0251109b0639be53c520e4d36304f662
Mood Game <Mr.yrban4k@gmail.com>
2025-04-04 20:16:16 +0300

    Update main.asm

--- Snake/main.asm
+++ Snake/main.asm
@@ -1,70 +1,70 @@
 .386
-include \masm32\include\masm32rt.inc  ; Подключение стандартных библиотек MASM32 для работы с консолью
+include \masm32\include\masm32rt.inc  
 
 .data
-newgame BYTE 'we are playing?(y/n)', 0  ; Сообщение для начала игры
-user_input BYTE 1 DUP(0)  ; Место для ввода пользователя
-newline BYTE 0Dh, 0Ah, 0  ; Символ новой строки
-star BYTE '-', 0  ; Символ для отображения игрового поля
-head BYTE '@', 0  ; Символ головы змейки
-count DWORD 30  ; Ширина игрового поля
-count1 DWORD 20  ; Высота игрового поля
-snak BYTE '*', 0  ; Символ еды
+newgame BYTE 'we are playing?(y/n)', 0  
+user_input BYTE 1 DUP(0)  
+newline BYTE 0Dh, 0Ah, 0 
+star BYTE '-', 0  
+head BYTE '@', 0  
+count DWORD 30  
+count1 DWORD 20  
+snak BYTE '*', 0  
 
-cord_X DWORD 3  ; Координата X головы змейки
-cord_Y DWORD 1  ; Координата Y головы змейки
-direction DWORD 1  ; Направление змейки (1 - вправо, 2 - вниз, 3 - влево, 4 - вверх)
+cord_X DWORD 3 
+cord_Y DWORD 1  
+direction DWORD 1  
 
-snake_X DWORD 100 DUP(0)  ; Массив для хранения X координат змейки
-snake_Y DWORD 100 DUP(0)  ; Массив для хранения Y координат змейки
+snake_X DWORD 100 DUP(0) 
+snake_Y DWORD 100 DUP(0) 
 
-snake_length DWORD 0  ; Длина змейки/количество очков
-score_text BYTE 'Score: ', 0  ; Текст для отображения счета
-score_str BYTE 10 DUP(0)  ; Строка для отображения счета
+snake_length DWORD 0 
+score_text BYTE 'Score: ', 0  
+score_str BYTE 10 DUP(0)  
 
-msg_title BYTE 'Сообщение', 0  ; Заголовок для msgbox
-msg_gameover BYTE 'Выполнил Детков Александр ПИб-2', 0  ; Сообщение о завершении игры
+msg_title BYTE 'YOU LOSE', 0  
+msg_gameover BYTE 'GAME OVER!', 0  
 
-food_X DWORD 100 DUP(0)  ; Массив для хранения X координат пищи
-food_Y DWORD 100 DUP(0)  ; Массив для хранения Y координат пищи
-food_count DWORD 0  ; Счетчик для пищи
+food_X DWORD 100 DUP(0)  
+food_Y DWORD 100 DUP(0) 
+food_count DWORD 0  
 
 .data?
-hConsoleOutput DWORD ?  ; Дескриптор консольного вывода
-dwWritten DWORD ?  ; Количество записанных символов
-NumberOfCharsWritten DWORD ?  ; Количество записанных символов
+hConsoleOutput DWORD ?  
+dwWritten DWORD ?  
+NumberOfCharsWritten DWORD ?  
 
-old_cord_X DWORD ?  ; Старая координата X головы змейки
-old_cord_Y DWORD ?  ; Старая координата Y головы змейки
+old_cord_X DWORD ?  
+old_cord_Y DWORD ?  
 
 .code
 
 start:
-    invoke AllocConsole  ; Выделение памяти для консоли
-    invoke GetStdHandle, STD_OUTPUT_HANDLE  ; Получение дескриптора для вывода в консоль
-    mov hConsoleOutput, eax  ; Сохранение дескриптора в переменную
-    call hide_cursor  ; Скрытие курсора для улучшения восприятия игры
+    invoke AllocConsole  
+    invoke GetStdHandle, STD_OUTPUT_HANDLE  
+    mov hConsoleOutput, eax  
+    call hide_cursor  
 
-    invoke WriteConsole, hConsoleOutput, addr newgame, SIZEOF newgame - 1, addr NumberOfCharsWritten, 0  ; Выводим сообщение "Начать игру?"
-    invoke GetStdHandle, STD_INPUT_HANDLE  ; Получаем дескриптор для ввода с клавиатуры
-    mov ebx, eax  ; Сохраняем дескриптор
-    invoke ReadConsole, ebx, addr user_input, 1, addr NumberOfCharsWritten, 0  ; Чтение ввода
+    invoke WriteConsole, hConsoleOutput, addr newgame, SIZEOF newgame - 1, addr NumberOfCharsWritten, 0  
+    invoke GetStdHandle, STD_INPUT_HANDLE  
+    mov ebx, eax 
+    invoke ReadConsole, ebx, addr user_input, 1, addr NumberOfCharsWritten, 0  
 
-    mov al, [user_input]  ; Чтение символа
-    cmp al, 'y'  ; Если введен 'y', начинаем игру
+    mov al, [user_input] 
+    cmp al, 'y' 
     je start_game
-    cmp al, 'n'  ; Если введен 'n', , завершаем игру
-    jne game_over  ; Если введен неверный символ, завершаем игру
+    cmp al, 'n'  
+    jne game_over  
 
 start_game:
-    mov eax, 0  ; Очищаем консоль
+    mov eax, 0  
     mov ecx, 80 * 25
     invoke FillConsoleOutputCharacter, hConsoleOutput, ' ', ecx, eax, addr dwWritten
     mov edx, 0
-    xor edx, edx  ; Сброс курсора в начало
+    xor edx, edx 
     invoke SetConsoleCursorPosition, hConsoleOutput, edx
 
-    mov ebx, count1  ; Рисуем игровое поле
+    mov ebx, count1  
     for_loop:
         mov ecx, count
         push ebx
@@ -81,11 +81,10 @@ start_game:
         cmp ebx, 0
         jg for_loop
 
-    call place_food  ; Генерируем пищу на поле
-    jmp snake_start  ; Переходим к циклу игры
+    call place_food 
+    jmp snake_start 
 
 snake_start:
-    ; Чтение состояния клавиш для изменения направления змейки
     invoke GetAsyncKeyState, VK_UP
     test ax, 8000h
     jnz set_direction_up
@@ -102,7 +101,6 @@ snake_start:
     test ax, 8000h
     jnz set_direction_right
 
-    ; В зависимости от направления выполняем движение змейки
     mov eax, direction
     cmp eax, 1
     je move_right
@@ -113,68 +111,66 @@ snake_start:
     cmp eax, 4
     je move_up
 
-    jmp snake_start  ; Продолжаем цикл
+    jmp snake_start  
 
 set_direction_up:
-    cmp direction, 2  ; Проверка, чтобы змейка не двигалась в противоположную сторону
+    cmp direction, 2  
     je snake_start
-    mov direction, 4  ; Устанавливаем новое направление (вверх)
+    mov direction, 4  
     jmp snake_start
 
 set_direction_down:
     cmp direction, 4
     je snake_start
-    mov direction, 2  ; Устанавливаем новое направление (вниз)
+    mov direction, 2 
     jmp snake_start
 
 set_direction_left:
     cmp direction, 1
     je snake_start
-    mov direction, 3  ; Устанавливаем новое направление (влево)
+    mov direction, 3 
     jmp snake_start
 
 set_direction_right:
     cmp direction, 3
     je snake_start
-    mov direction, 1  ; Устанавливаем новое направление (вправо)
+    mov direction, 1  
     jmp snake_start
 
 move_up:
-    dec cord_Y  ; Двигаем змейку вверх
+    dec cord_Y 
     mov eax, cord_Y
-    cmp eax, 0  ; Проверка на столкновение с верхней границей
+    cmp eax, 0 
     jl game_over
     jmp update_snake
 
 move_down:
-    inc cord_Y  ; Двигаем змейку вниз
+    inc cord_Y 
     mov eax, cord_Y
-    cmp eax, count1  ; Проверка на столкновение с нижней границей
+    cmp eax, count1 
     jge game_over
     jmp update_snake
 
 move_left:
-    dec cord_X  ; Двигаем змейку влево
+    dec cord_X  
     mov eax, cord_X
-    cmp eax, 0  ; Проверка на столкновение с левой границей
+    cmp eax, 0 
     jl game_over
     jmp update_snake
 
 move_right:
-    inc cord_X  ; Двигаем змейку вправо
+    inc cord_X  
     mov eax, cord_X
-    cmp eax, count  ; Проверка на столкновение с правой границей
+    cmp eax, count 
     jge game_over
     jmp update_snake
 
 update_snake:
-    ; Обновляем положение змейки на экране
     mov eax, cord_X
     mov old_cord_X, eax
     mov eax, cord_Y
     mov old_cord_Y, eax
 
-    ; Проверка на съеденную пищу
     mov eax, cord_X
     cmp eax, [food_X]
     jne not_eaten
@@ -182,14 +178,13 @@ update_snake:
     cmp eax, [food_Y]
     jne not_eaten
 
-    call place_food  ; Генерируем новую пищу
+    call place_food  
     mov eax, snake_length
     inc eax
-    mov snake_length, eax  ; Увеличиваем длину змейки
+    mov snake_length, eax  
     jmp continue_game
 
 not_eaten:
-    ; Отображаем голову змейки
     mov eax, cord_X
     mov edx, cord_Y
     shl edx, 16
@@ -198,13 +193,12 @@ not_eaten:
     invoke WriteConsole, hConsoleOutput, addr head, SIZEOF head - 1, addr NumberOfCharsWritten, 0
 
 continue_game:
-    invoke Sleep, 100  ; Пауза для замедления движения змейки
-    call clean_snake  ; Очищаем старую позицию змейки
-    call display_score  ; Отображаем текущий счет
-    jmp snake_start  ; Продолжаем цикл игры
+    invoke Sleep, 100  
+    call clean_snake 
+    call display_score  
+    jmp snake_start 
 
 clean_snake:
-    ; Очищаем старую позицию змейки
     mov eax, old_cord_X
     mov edx, old_cord_Y
     shl edx, 16
@@ -214,7 +208,6 @@ clean_snake:
     ret
 
 place_food:
-    ; Генерация новой пищи на поле
     invoke GetTickCount
     mov eax, [food_count]
     add eax, 1
@@ -234,7 +227,6 @@ place_food:
     div ecx
     mov [food_Y], edx  
 
-    ; Отображаем пищу на экране
     mov eax, [food_X]
     mov edx, [food_Y]
     shl edx, 16
@@ -246,11 +238,10 @@ place_food:
 
 game_over:
 
-    invoke MessageBoxA, 0, addr msg_gameover, addr msg_title, MB_OK  ; Сообщение о завершении игры
-    invoke ExitProcess, 0  ; Завершение программы
+    invoke MessageBoxA, 0, addr msg_gameover, addr msg_title, MB_OK 
+    invoke ExitProcess, 0 
 
 hide_cursor:
-    ; Скрытие курсора
     sub esp, sizeof CONSOLE_CURSOR_INFO
     mov eax, hConsoleOutput
     mov dword ptr [esp], 1
@@ -260,7 +251,6 @@ hide_cursor:
     ret
 
 display_score:
-    ; Отображение текущего счета
     mov eax, count1
     mov edx, count
     mov ecx, 1
@@ -271,13 +261,12 @@ display_score:
     invoke WriteConsole, hConsoleOutput, addr score_text, SIZEOF score_text - 1, addr NumberOfCharsWritten, 0
 
     mov eax, snake_length
-    call itoa  ; Преобразование числа в строку
+    call itoa 
 
     invoke WriteConsole, hConsoleOutput, addr score_str, SIZEOF score_str - 1, addr NumberOfCharsWritten, 0
     ret
 
 itoa:
-    ; Преобразование целого числа в строку
     mov ebx, 10
     lea ecx, score_str
     itoa_loop:
